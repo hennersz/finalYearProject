a77907b6442d745f873dfdd85d92a41faf9620c1
Henry Mortimer <henry@morti.plus.com>
2017-04-07 13:45:37 +0100

    added key manager test

--- tests/keyManager_test.py
+++ tests/keyManager_test.py
@@ -5,6 +5,7 @@ from twisted.internet import defer
 from p2ppki.backend.keyManager import KeyManager
 from p2ppki.utils import hashToB64
 from helpers import FakeDHT, createKeystore
+from pisces.spkilib import spki
 import mock
 import pytest
 
@@ -28,3 +29,37 @@ def test_insert(ks):
     value = str(keys[0][0].sexp().encode_canonical())
     dht.set.assert_called_with(key, value)
     assert res
+
+
+@pytest.inlineCallbacks
+def test_get(ks):
+    keystore = ks[0]
+    keys = ks[1]
+    pubs = [x[0] for x in keys]
+    dht = FakeDHT(pubs[1:], [], keystore)
+
+    k = KeyManager(dht, keystore)
+
+    res = yield k.getKey(keys[0][0].getPrincipal())
+    assert res is None
+
+    res = yield k.getKey(keys[1][0].getPrincipal())
+    assert isinstance(res, spki.PublicKey)
+    assert res.getPrincipal() == keys[1][0].getPrincipal()
+
+
+def test_listLocalKeys(ks):
+    keystore = ks[0]
+    keys = ks[1]
+    pubs = [x[0] for x in keys]
+    dht = FakeDHT(pubs[1:], [], keystore)
+
+    k = KeyManager(dht, keystore)
+
+    pubs, privs = k.listLocalKeys()
+    assert len(pubs) == 5
+    assert privs is None
+
+    pubs, privs = k.listLocalKeys(True)
+    assert len(pubs) == 5
+    assert len(privs) == 5
